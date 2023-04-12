725a3d89a8958d8067cd40c8e82fdcd024252983
Neville <wee.neville@ymail.com>
2023-04-12 11:57:05 +0800

    Initialize repo

---
+++ student_files/q1.py
@@ -0,0 +1,13 @@
+import sys
+from pyspark.sql import SparkSession
+# you may add more import if you need to
+
+
+# don't change this line
+hdfs_nn = sys.argv[1]
+
+spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
+# YOUR CODE GOES BELOW
+df = spark.read.option("header",True).csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+df.printSchema()
+
