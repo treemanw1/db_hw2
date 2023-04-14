a003fef0ce341895a91884fb7d74641ec6b1f2ce
pinkadotted <subkmrspm@gmail.com>
2023-04-14 23:23:13 +0800

    Edited q1.py. Please check if it works.

--- student_files/q1.py
+++ student_files/q1.py
@@ -9,16 +9,18 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
-df = (
-    spark.read.option("header", True)
-    .option("inferSchema", True)
-    .option("delimiter", ",")
-    .option("quotes", '"')
-    .csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+
+# Reading the CSV file into a DataFrame
+df = (spark.read.option("header", True).option("inferSchema", True).option("delimiter", ",").option("quotes", '"').csv(f"hdfs://{hdfs_nn}:9000/assignment2/part1/input/"))
+
+# Cleaning up the DataFrame by removing rows with no reviews or rating < 1.0 or no rating
+cleaned_df = df.filter(
+    (col("Rating") >= 1.0) & (col("Rating").isNotNull()) & (col("Reviews").isNotNull())
 )
 
-df = df.filter(
-    (col("Rating") >= 1.0) & (col("Rating").isNotNull())
-)
+# Writing the cleaned DataFrame as CSV into an HDFS path
+df.coalesce(1).write.mode('overwrite').csv(f"hdfs://{hdfs_nn}:9000/assignment2/output/question1/", header=True)
 
-df.coalesce(1).write.mode('overwrite').csv("hdfs://%s:9000/assignment2/output/question1/" % (hdfs_nn), header=True)
+
+# Stopping the SparkSession
+spark.stop()
